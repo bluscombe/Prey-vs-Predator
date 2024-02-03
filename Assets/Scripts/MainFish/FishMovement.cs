9d118376cccf91db7468a6868740625843b11528
bluscombe <113560030+bluscombe@users.noreply.github.com>
2024-02-03 17:45:12 -0500

    Folder arrangement

--- Assets/Scripts/MainFish/FishMovement.cs
+++ Assets/Scripts/MainFish/FishMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FishMovement : MonoBehaviour
 {
-    public float speed = 5.0f;
+    public float speed = 1.0f;
     private Vector2 screenBounds;
     private float targetYRotation; // Target rotation around the Y-axis
     public float rotationSpeed = 5.0f; // Speed of rotation
