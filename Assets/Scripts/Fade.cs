ed574dcb10357f20c6e007c7fd917f166aa84b4b
stonechang01 <30443681+stonechang01@users.noreply.github.com>
2024-04-14 19:16:03 -0400

    fade screen implemented

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fade : MonoBehaviour
+{
+    public Animator animator;
+
+    public void fadeIn(){
+        animator.SetTrigger("FadeIn");
+    }
+
+    public void fadeOut(){
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void fadeOutToGame(){
+        SceneManager.LoadScene("Game Scene");
+    }
+}
